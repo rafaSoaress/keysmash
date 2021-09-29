c2411449d46a0032efaa88f8a8e01d9cc75429dc
rafaSoaress <rafaelsoares270400@gmail.com>
2021-09-29 13:06:58 +0200

    added pages views for, Discord server (will take users to KeySmash Discord server), Login and also Register

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="flex flex-col w-full px-4 py-3 font-sans text-center bg-gray-800 shadow sm:flex-row sm:text-left sm:justify-between sm:items-baseline">
       <div className="mb-2 sm:mb-0">
-        <a href="/" className="text-2xl text-white no-underline hover:text-yellow-400">Home</a>
+        <a href="/" className="text-2xl text-white no-underline hover:text-yellow-400">KeySmash</a>
       </div>
       <div>
         <a href="/discord" className="ml-2 text-lg text-white no-underline hover:text-yellow-400">Discord</a>
--- pages/discord.tsx
+++ pages/discord.tsx
@@ -1,11 +1,11 @@
 import Layout from "../components/Layout";
 
-//Here will go the page view that will take the users to Discord server.
+//Here will be the view page that takes the users to the Discord server of KeySmash.
 
 const Discord = () => {
   return (
     <Layout>
-      <div className="py-8 text-5xl text-center text-white">Discord server here</div>
+      <div className="py-8 text-5xl text-center text-white">Discord server</div>
     </Layout>
   );
 };
